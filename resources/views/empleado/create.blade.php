7d74fad3f448085ee5eb296f790b8c61b2b42d2c
edwin2568 <edwinmunoz804@gmail.com>
2021-12-03 15:00:59 -0500

    Validacion laravel

---
+++ resources/views/empleado/create.blade.php
@@ -0,0 +1,9 @@
+Formulario de creacion de empleados
+
+<form action="{{url("/empleado") }}" method="post" enctype="multipart/form-data">
+@csrf
+@include('empleado.form', ['modo'=>'Crear'])
+
+
+
+</form>
